00648d7ed39a972c9fa5e48813e0f93c7eb18170
AhmedSennary <67074285+AhmedSennary@users.noreply.github.com>
2022-09-20 20:24:35 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+
+#include "stdio.h"
+#include "app.h"
+#include "card.h"
+#include "server.h"
+#include "terminal.h"
+#include "string.h"
+#include "stdlib.h"
+#define test(x) (!((x)&((x)-1)))
+
+int main() {
+	int i = test(5);
+		int j = 0;
+
+	return 0 ;
+}
